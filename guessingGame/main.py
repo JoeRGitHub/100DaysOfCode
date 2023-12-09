719e2a27c4508ac6b43bda56338da5b3e164c940
Yossi Rozen <yossi.rozen@morphisec.com>
2023-12-09 20:40:00 +0200

    finsh game + art logo

--- guessingGame/main.py
+++ guessingGame/main.py
@@ -1,19 +1,3 @@
-# Scoope:
-# enemies = 1
-
-# def increase_enemies():
-#   enemies = 2
-#   print(f"enemies inside function: {enemies}")
-
-# increase_enemies()
-# print(f"enemies outside function: {enemies}")
-
-# def bar():
-#     if 16 > 9:
-#       my_variable = 16
-#     print(my_variable)
- 
-# bar()
 
 #Number Guessing Game Objectives:
 
@@ -25,33 +9,25 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
-##Get a randome number from 1 to 100 and save it in a ver
-
 import random
 rand_num = random.randint(1, 100) #60
-print(rand_num)
-# Choice game leval #Easy=10 guess #Hard=5 guess
+from art import logo
 
-easy = 1
+easy = 10
 hard = 5
 
 def choice_game_lavel(easy,hard):
+    print(logo)
     leval_game = input("Choice game leval 'Easy' or 'Hard': ")
-    #gusee_num()
-    #out = int(input('Guess a number between 1 to 100: '))
-    # print(type(out))
-    # print(type(rand_num))
-
+    out = int(input('Guess a number between 1 to 100: '))
     if leval_game == 'easy':
-        #number = input('Guess a number between 1 to 100: ')
-        print(f'out befroe while: {gusee_num()}')
-        while gusee_num() != rand_num and easy > 0:
-            print(f'out after while:{gusee_num()}')
-            print(easy)
-            print(rand_num)
+        while out != rand_num and easy > 0:
             easy -= 1
-            #out = int(input('Guess a number between 1 to 100: '))
-            #gusee_num()
+            if rand_num > out:
+              print("To low.")
+            elif rand_num < out:
+              print("To high.")
+            out = int(input('Guess a number between 1 to 100: '))
         else:
             if easy == 0:
                 print('no more guess!')
@@ -59,15 +35,14 @@ def choice_game_lavel(easy,hard):
                 print('nice guess!')
     
     elif leval_game == 'hard':
-        #number = input('Guess a number between 1 to 100: ')
         print(f'out befroe while: {out}')
         while out != rand_num and hard > 0:
-            print(f'out after while:{out}')
-            print(hard)
-            print(rand_num)
             hard -= 1
+            if rand_num > out:
+              print("To low.")
+            elif rand_num < out:
+              print("To high.")            
             out = int(input('Guess a number between 1 to 100: '))
-            #gusee_num()
         else:
             if hard == 0:
                 print('no more guess!')
@@ -78,4 +53,3 @@ def gusee_num():
     return input('Guess a number between 1 to 100: ')
 
 choice_game_lavel(easy,hard)
-#gusee_num()
