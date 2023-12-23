cef8bf68a6a0edb808f4883df0b401bce03c93a7
Yossi Rozen <yossi.rozen@morphisec.com>
2023-12-23 21:14:59 +0200

    fix flow order

--- coffeeMachine/main.py
+++ coffeeMachine/main.py
@@ -8,24 +8,12 @@ def checkResources(userChoice):
 
     if userChoice == 'espresso':
         if water <= resources['water'] and coffee <= resources['coffee']:
-            waterSum = resources['water'] - water
-            coffeeSum = resources['coffee'] - coffee
-            resources['water'] = waterSum
-            resources['coffee'] = coffeeSum
-            print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}")
             processCoins(userChoice)
         else:
             print('Not enough')
     elif userChoice == 'cappuccino' or userChoice == 'latte':
         milk = MENU[userChoice]['ingredients']['milk']
         if water <= resources['water'] and milk <= resources['milk'] and coffee <= resources['coffee']:
-            waterSum = resources['water'] - water
-            milkSum = resources['milk'] - milk
-            coffeeSum = resources['coffee'] - coffee
-            resources['water'] = waterSum
-            resources['milk'] = milkSum
-            resources['coffee'] = coffeeSum
-            print(f"Remaining resources:\nWater: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']} \nMoney: {resources['money']}")
             processCoins(userChoice)
         else:
             print('Not enough')
@@ -38,27 +26,40 @@ def processCoins(userChoice):
     dimes = 0.10
     nickles = 0.05
     pennies = 0.01
-
-    #cashRegister = int(input("Please insert coins:\n(quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01)"))
     cashQuarters = int(input("Quarters(0.25): ")) * quarters
     cashDimes = int(input("Dimes(0.10): ")) * dimes
     cashNickles = int(input("Nickles(0.05): ")) * dimes
     cashPennies = int(input("Pennies(0.01): ")) * pennies
     x = cashQuarters + cashDimes + cashNickles + cashPennies
-    print(f'flow {x}')
-    #  = 10 * 0.25 = 2.5
-    #  = 2.5 - 3.0 = 0.5
-    #new_cost = x - cost
+    print(f'flow {round(x,2)}')
     if x < cost:
         print("Sorry that's not enough money. Money refunded.")
         userOrder()
     else:
-        processCoins(userChoice)
         refund = x - cost
-        resources['money']  += cost
+        resources['money'] += cost
         print(f"Custmer refund: {round(refund,2)}, Cash register: {resources['money']}")
-#TODO: Process order
+        userDrinkOrder(userChoice)
 
+#TODO: Process order
+def userDrinkOrder(userChoice):
+    water = MENU[userChoice]['ingredients']['water']
+    coffee = MENU[userChoice]['ingredients']['coffee']
+    cost = MENU[userChoice]['cost']
+
+    if userChoice == 'latte' and 'cappuccino':
+        milk = MENU[userChoice]['ingredients']['milk']
+        waterSum = resources['water'] - water
+        coffeeSum = resources['coffee'] - coffee
+        resources['water'] = waterSum
+        resources['coffee'] = coffeeSum
+        print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}")
+    elif userChoice == 'espresso':
+        waterSum = resources['water'] - water
+        coffeeSum = resources['coffee'] - coffee
+        resources['water'] = waterSum
+        resources['coffee'] = coffeeSum
+        print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}")
 
 #TODO: ask user to choice an order
 def userOrder():
@@ -70,18 +71,7 @@ def userOrder():
         elif userChoice == 'report':
             for i in resources:
                 print(i, resources[i])
-        # elif userChoice == 'espresso' or 'latte' or 'cappuccino':
-        #     processCoins(userChoice)
-        #     checkResources(userChoice)
         else:
-            #print(MENU[userChoice])
             checkResources(userChoice)
-
 userOrder()
-
-
-# for key, value in resources.items():
-#     if value.startswith('bo'):
-#         print(key, value)
-
 
