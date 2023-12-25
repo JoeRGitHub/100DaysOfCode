b507f604293e8377469a41bd89ddb726342b4315
Yossi Rozen <yossi.rozen@morphisec.com>
2023-12-25 11:56:06 +0200

    remove not on use code

--- coffeeMachine/coffeeMachineData.py
+++ coffeeMachine/coffeeMachineData.py
@@ -30,11 +30,3 @@ resources = {
     "coffee": 100,
     "money": 0,
 }
-
-# Pennies(0.01): 102.3
-# Money paid: 5.58
-# Custmer refund: 4.08, Cash register: 1.5
-# Water: 250
-# Milk: 200
-# Coffee: 82
-# What would you like? (espresso/latte/cappuccino, off | report):
