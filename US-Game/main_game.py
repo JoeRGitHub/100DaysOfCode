732cfb0e8d345f0182cde62ea2e8165c739b39dc
Yossi Rozen <yossi.rozen@morphisec.com>
2024-04-17 17:16:33 +0300

    finished most of the game

--- US-Game/main_game.py
+++ US-Game/main_game.py
@@ -2,50 +2,50 @@ import turtle
 from pandas import *
 import string
 
-screen = turtle.Screen()
-screen.title("US State Game")
-image = "blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
-turtle.setup(725, 491)
-turtle.penup()
-screen.tracer(0) # empty screen
-
-number_of_guess = 0
-trace_guess = 0
-
+def setup_screen():
+    screen = turtle.Screen()
+    screen.title("US State Game")
+    image = "blank_states_img.gif"
+    screen.addshape(image)
+    turtle.shape(image)
+    turtle.setup(725, 491)
+    turtle.penup()
+    screen.tracer(0) # empty screen
 
 
 FONT_SIZE = 15
 turtle.color("red")
-
 df = read_csv('50_states.csv')
 
-# # monday = df[df.state == answer]
-# # print(monday)
+def main():
+    screen = turtle.Screen()
+    number_of_guess = 0
+    guess = []
 
+    while number_of_guess < 50:
+        # state_answer = string.capwords(screen.textinput(f"{number_of_guess}/50", "Guess USAs 50 states:"))
+        state_answer = screen.textinput(f"{number_of_guess}/50", "Guess USAs 50 states:").title()
+        print(state_answer)
 
-while number_of_guess < 50:
+        if state_answer == "Exit":
+            break
 
-    state_answer = string.capwords(screen.textinput(f"{number_of_guess}/50", "Guess USAs 50 states:"))
-    print(state_answer)
+        if state_answer in df['state'].values:
+            print("Good guess!")
+            state_data = df[df.state == state_answer]
+            # I have add "iloc[0]" because single element Series is deprecated
+            turtle.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
+            turtle.write(state_answer, font=("Arial", FONT_SIZE, "bold"), align="center")
+            number_of_guess += 1
+            print(f'Debug: {state_answer}')
+            guess.append(state_answer)
+            print(guess)
+        else:
+            print("Incorrect guess. Try again!")
 
-    if state_answer in df['state'].values:
-        print("You won!")
-        #print(df['x'].values)
-        x = df[df.state == state_answer]
-        #x = df['x'].tolist() == state_answer
-        y = x.values.tolist()
-        #y = x.to_dict
-        print(x)
-        print(f'the X: {y[0][1]}')
-        print(f'the Y: {y[0][2]}')
-        turtle.goto(y[0][1], y[0][2])
-        turtle.write(state_answer, font=("Arial", FONT_SIZE, "bold"), align="center")
-        number_of_guess += 1
-    else:
-        print("You lost!")
+    # show new screen after 'screen.tracer(0)'
+    screen.update()
 
-
-screen.update() # show new screen after 'screen.tracer(0)'
-screen.exitonclick()
+if __name__ == "__main__":
+    setup_screen()
+    main()
