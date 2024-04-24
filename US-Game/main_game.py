a379912fe2decfcb7d8b776acefb512ade24f19c
Yossi Rozen <yossi.rozen@morphisec.com>
2024-04-24 15:38:30 +0300

    export remind_states list to csv, check if state is in list and more

--- US-Game/main_game.py
+++ US-Game/main_game.py
@@ -1,6 +1,6 @@
 import turtle
-from pandas import *
-import string
+# from pandas import *
+import pandas as pd
 
 def setup_screen():
     screen = turtle.Screen()
@@ -15,36 +15,52 @@ def setup_screen():
 
 FONT_SIZE = 15
 turtle.color("red")
-df = read_csv('50_states.csv')
+df = pd.read_csv('50_states.csv')
+
 
 def main():
     screen = turtle.Screen()
     number_of_guess = 0
-    guess = []
+    guess_list = []
+    update_list = []
 
     while number_of_guess < 50:
-        # state_answer = string.capwords(screen.textinput(f"{number_of_guess}/50", "Guess USAs 50 states:"))
         state_answer = screen.textinput(f"{number_of_guess}/50", "Guess USAs 50 states:").title()
         print(state_answer)
 
         if state_answer == "Exit":
+            for i in df.state:
+                if i not in guess_list:
+                    update_list.append(i)
+            # Create a new CSV file form the reminder state
+            # The 'w+' overwrite the exists file
+            new_csv = pd.DataFrame(update_list)
+            new_csv.to_csv("remind_states.csv", mode='w+')
+
+            print(guess_list)
+            print(f'List of missing countries:\n{update_list}')
             break
 
-        if state_answer in df['state'].values:
+        if state_answer in df['state'].values and state_answer not in guess_list:
             print("Good guess!")
             state_data = df[df.state == state_answer]
-            # I have add "iloc[0]" because single element Series is deprecated
+            # I have added "iloc[0]" because single element Series is deprecated
             turtle.goto(int(state_data.x.iloc[0]), int(state_data.y.iloc[0]))
             turtle.write(state_answer, font=("Arial", FONT_SIZE, "bold"), align="center")
             number_of_guess += 1
             print(f'Debug: {state_answer}')
-            guess.append(state_answer)
-            print(guess)
+            guess_list.append(state_answer)
+            print(guess_list)
         else:
             print("Incorrect guess. Try again!")
 
+    if number_of_guess == 50:
+        print("You won!")
+    else:
+        print("You lost!")
     # show new screen after 'screen.tracer(0)'
     screen.update()
+
 
 if __name__ == "__main__":
     setup_screen()
