462164ac2bb68468a52bd9e6f2cd7e5f502a9a15
Yossi Rozen <yossi.rozen@morphisec.com>
2024-09-25 13:49:02 +0300

    add

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -8,23 +8,34 @@
 # Working with pands
 import pandas
 
+# step one: create a dict from the CSV
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
+#print(df)
 index_phonetic_alphabet = df.set_index('letter')['code'].to_dict()
 #print(index_phonetic_alphabet)
 
+# index_phonetic_alphabet_com = {key: value for (key, value) in df.items()}
+# print(index_phonetic_alphabet_com)
 name = input("Enter the name you need in your alphabet: ")
 #name ='didiy'
 
 new_phonetic_name = []
 
+# step two: create a loop that will run on the dict
 for letter in name.upper():
     for code_letter, code_names in index_phonetic_alphabet.items():
         if letter == code_letter:
             new_phonetic_name.append(code_names)
-print(new_phonetic_name)
+print('For loop:', new_phonetic_name)
+#
+# name_letters = [code_names for letter in name.upper() for (code_letter, code_names) in index_phonetic_alphabet.items()
+#                 if letter == code_letter]
+# print('List Comprehension:', name_letters)
 
-name_letters = [code_names for letter in name.upper() for (code_letter, code_names) in index_phonetic_alphabet.items()
-                if letter == code_letter]
-print(name_letters)
+# Try to create the dictionary comprehension without using the 'index_phonetic_alphabet'
+
+
+
+
 
 
